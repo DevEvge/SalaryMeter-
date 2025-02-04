ded012f5fccfe3bb8607f39630f2da9bc995f356
DevEvge <evgeniivolkov18@gmail.com>
2025-02-04 21:46:02 +0200

    Add arrow_back imagebutton

--- app/src/main/java/com/example/jobtracker/NewDataActivity.java
+++ app/src/main/java/com/example/jobtracker/NewDataActivity.java
@@ -1,9 +1,16 @@
 package com.example.jobtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +27,31 @@ public class NewDataActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageButton arrowBack = findViewById(R.id.arrow_back);
+        arrowBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewDataActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        SwitchCompat switchBTN = findViewById(R.id.switchbtn);
+        switchBTN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                EditText input = findViewById(R.id.additionalInput);
+                if (isChecked) {
+                    input.setEnabled(true);
+                } else {
+                    input.setEnabled(false);
+                }
+            }
+        });
+
+
+
+
     }
 }
