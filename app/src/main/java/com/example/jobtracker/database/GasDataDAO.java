2db94546553cf9b6c83b9da1bf8117eb1242cd13
DevEvge <evgeniivolkov18@gmail.com>
2025-02-18 22:30:52 +0200

    Fix bug with clean DB

--- app/src/main/java/com/example/jobtracker/database/GasDataDAO.java
+++ app/src/main/java/com/example/jobtracker/database/GasDataDAO.java
@@ -15,7 +15,7 @@ public interface GasDataDAO {
     @Query("SELECT * FROM GasData WHERE date LIKE :yearMonth || '%'")
     List<GasData> getAllByYearMonth(String yearMonth);
 
-    @Query("DELETE FROM DayData")
+    @Query("DELETE FROM GasData")
     void deleteAll();
 
 }
